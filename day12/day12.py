3788b15703c20121f6388f9a888a0e95417e1eb5
MichaelBo1 <92109316+MichaelBo1@users.noreply.github.com>
2024-12-12 13:48:34 +0000

    Return region coords instead of area for bfs

--- day12/day12.py
+++ day12/day12.py
@@ -22,7 +22,7 @@ MMMISSJEEE
 
 MARKED_SQUARE = "#"
 
-def calculate_price(plot: List[List[str]]):
+def calculate_price(plot: List[List[str]]) -> Tuple[int, int]:
     rows, cols = len(plot), len(plot[0])
     directions = [[1,0], [-1,0], [0,1], [0,-1]]
     visited: Set[Tuple[int,int]] = set()
@@ -30,44 +30,41 @@ def calculate_price(plot: List[List[str]]):
     def in_bounds(r: int, c: int) -> bool:
         return 0 <= r < rows and 0 <= c < cols
      
-    def bfs_region(r: int, c: int) -> Tuple[int, int]:
-        q: Deque[Tuple[int,int]] = deque()
-        area = 1
+    def bfs_region(r: int, c: int) -> Tuple[int, Set[Tuple[int, int]]]:
+        q: Deque[Tuple[int, int]] = deque()
+        region: Set[Tuple[int, int]] = set()
         perimeter = 0
         plant_type = plot[r][c]
 
         q.append((r,c))
         visited.add((r,c))
+        region.add((r,c))
 
         while q:
             row, col = q.popleft()
-            connections = 0 
             for dr, dc in directions:
                 newr, newc = row + dr, col + dc
-                if not in_bounds(newr, newc):
-                    continue
 
-                if plot[newr][newc] != plant_type:
+                if not in_bounds(newr, newc) or plot[newr][newc] != plant_type:
+                    perimeter += 1
                     continue
                 
-                connections += 1
-
                 if (newr, newc) not in visited:
                     q.append((newr, newc))
                     visited.add((newr, newc))
-                    area += 1
-            perimeter += 4 - connections
+                    region.add((newr, newc))
         
-        return area, perimeter
+        return region, perimeter
 
-    cost = 0
+    p1 = 0
+    p2 = 0
     for row in range(rows):
         for col in range(cols):
             if (row, col) not in visited:
-                area, perimeter = bfs_region(row, col)
-                cost += area * perimeter
+                region, perimeter = bfs_region(row, col)
+                p1 += len(region) * perimeter
     
-    return cost
+    return p1, p2
 
 
 
@@ -78,7 +75,8 @@ def main() -> None:
         raw = open(sys.argv[1], "r").read().splitlines()
 
     puzzle = [list(line) for line in raw]
-    print(f"Part 1: {calculate_price(puzzle)}")
+    p1, p2 = calculate_price(plot=puzzle)
+    print(f"Part 1: {p1}")
 
         
 if __name__ == "__main__":
